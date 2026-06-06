1b79724e2403c1338b6ff8b31a04fe453ac28367
jiayun <jiayun.work@gmail.com>
2026-06-07 01:06:54 +0800

    处理编译问题

--- backend/src/scripts/addContactPhoneColumn.js
+++ backend/src/scripts/addContactPhoneColumn.js
@@ -1,4 +1,4 @@
-import { sequelize } from '../config/database.js';
+import sequelize from '../config/database.js';
 import { QueryTypes } from 'sequelize';
 
 async function addContactPhoneColumn() {
